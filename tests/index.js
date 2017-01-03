d79312c1f35c6b51e2221da0c8e2edabfc0f196b
slode <rob.m@myplanet.com>
2017-01-03 14:51:23 -0500

    move fullTest() into actual test file and remove from main module

--- tests/index.js
+++ tests/index.js
@@ -1,43 +1,92 @@
 'use strict'
 
 const test = require('tape')
+const base64 = require('base64-js')
 const redveil = require('../src/redveil')
-const crypto = require('crypto')
-
-test('sanity test', t => {
-  t.plan(2)
-  t.equal(typeof redveil.sanity, 'function')
-  t.equal(redveil.sanity(), 'it worked!')
-})
-
-test('dh', t => {
-  t.plan(1)
-
-  // genreate alice's keys...
-  const alice = crypto.createDiffieHellman(512)
-  const alice_key = alice.generateKeys()
-
-  // generate bob's keys...
-  const bob = crypto.createDiffieHellman(alice.getPrime(), alice.getGenerator())
-  const bob_key = bob.generateKeys()
-
-  // exchange and generate shared secret...
-  const alice_secret = alice.computeSecret(bob_key)
-  const bob_secret = bob.computeSecret(alice_key)
-
-  const alice_secret_str = alice_secret.toString('hex')
-  const bob_secret_str = bob_secret.toString('hex')
-
-  console.log('alice secret: ', alice_secret_str)
-  console.log('bob secret: ', bob_secret_str)
-
-  t.equal(alice_secret_str, bob_secret_str)
-})
 
 test('full test', t => {
   t.plan(1)
 
-  redveil.fullTest()
+  console.log('-----------------')
+  console.log('choose crypto lib')
+  console.log('-----------------')
 
-  t.equal(1, 1)
+  const cryptolib = redveil.chooseCrypto()
+
+  console.log('webcrypto: ', redveil.hasWebCrypto())
+  console.log('nodecrypto: ', redveil.hasNodeCrypto())
+  console.log('chosen cryptolib: ', cryptolib)
+
+  // generate keys
+  console.log('---------------------------')
+  console.log('generating shared secret...')
+  console.log('---------------------------')
+
+  // Create pub/priv keys for alice
+  const aliceKeys = redveil.keyPair(redveil.generateRandomBytes({
+    lib: cryptolib,
+    size: 32
+  }))
+  const alice_secretKeyStr = base64.fromByteArray(aliceKeys.secretKey)
+  const alice_publicKeyStr = base64.fromByteArray(aliceKeys.publicKey)
+
+  console.log('alice secret: ', alice_secretKeyStr)
+  console.log('alice pub: ', alice_publicKeyStr)
+
+  // Create pub/priv keys for bob
+  const bobKeys = redveil.keyPair(redveil.generateRandomBytes({
+    lib: cryptolib,
+    size: 32
+  }))
+  const bob_secretKeyStr = base64.fromByteArray(bobKeys.secretKey)
+  const bob_publicKeyStr = base64.fromByteArray(bobKeys.publicKey)
+
+  console.log('bob secret: ', bob_secretKeyStr)
+  console.log('bob pub: ', bob_publicKeyStr)
+
+  // Using new buffers for public keys create shared secret from them.
+  const alice_sharedSecret = redveil.sharedSecret(aliceKeys.secretKey, bobKeys.publicKey)
+  const bob_sharedSecret = redveil.sharedSecret(bobKeys.secretKey, aliceKeys.publicKey)
+
+  const alice_sharedSecretStr = base64.fromByteArray(alice_sharedSecret)
+  const bob_sharedSecretStr = base64.fromByteArray(bob_sharedSecret)
+
+  console.log('alice shared secret: ', alice_sharedSecretStr)
+  console.log('bob shared secret: ', bob_sharedSecretStr)
+
+  // encrypt/decrypt a message
+  console.log('--------------')
+  console.log('get plain text')
+  console.log('--------------')
+
+  const plainMsg = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only."
+
+  console.log('alice\'s plain text message: \n', plainMsg)
+
+  // Encrypt, and then decrypt, using browser's WebCrypto API
+  redveil.encrypt({
+    lib: cryptolib,
+    msg: plainMsg,
+    key: alice_sharedSecretStr,
+    compressed: true,
+    alg: 'aes-cbc-hmac'
+  })
+    .then(encryptedObj => {
+      console.log('encrypted message: \n', encryptedObj.data)
+
+      return redveil.decrypt({
+        lib: cryptolib,
+        msg: encryptedObj.data,
+        key: bob_sharedSecretStr,
+        iv: encryptedObj.iv,
+        mac: encryptedObj.mac,
+        compressed: true,
+        alg: 'aes-cbc-hmac'
+      })
+    })
+    .then(decryptedObj => {
+      console.log('bob\'s decrypted message: \n', decryptedObj.data)
+      t.equal(decryptedObj.data, plainMsg)
+    })
+    .catch(err => console.log('something went wrong: ', err))
 })
